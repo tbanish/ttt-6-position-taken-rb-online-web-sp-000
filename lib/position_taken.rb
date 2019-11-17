d011a090692014565b692ecc1b3161c51cc2ca67
Tom Banish <tombanish@gmail.com>
2019-11-17 16:54:23 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
 def position_taken?(board, index)
   if board[index] != " " || board[index] != "" || board[index] != nil
     true
-  elsif board[index] == " " || board[index] == "" || board[index] == nil
+  else board[index] == " " || board[index] == "" || board[index] == nil
     false
   end
 end
