de09c7f9683a442dd73ce02e0ace67ee8a2877c6
Tom Banish <tombanish@gmail.com>
2019-11-17 16:57:18 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,8 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  if board[index] != " " || board[index] != "" || board[index] != nil
-    false
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    FALSE
   else board[index] == " " || board[index] == "" || board[index] == nil
-    
+    TRUE
   end
 end
